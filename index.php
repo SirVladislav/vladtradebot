40481a79417e92a7e25390b6ed49a225295924b3
sirvladislav <sir.vladislav.programming@gmail.com>
2022-06-12 17:41:15 +0300

    2

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 require 'vendor/autoload.php';
-
+print("asdsdasdaa");
 $api = new Binance\API("ewbwdRMN7sJw5KByJnYPOSdPXP7vCwMzBZoqOdN5BlLELnP3mQSoeQ8bninkgUx4","2tc1wt1lvKt5tPTVMPmgPca0FfTNi6vYlcDA4DgHYvgYD4ZUUygbuXPHat3pCABa");
 print"<pre>";
 print_r($_POST);
