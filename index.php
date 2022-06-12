dc71b7e9112740029d7e5c70181d0e7274b18357
sirvladislav <sir.vladislav.programming@gmail.com>
2022-06-12 17:28:52 +0300

    2

--- index.php
+++ index.php
@@ -2,8 +2,6 @@
 require 'vendor/autoload.php';
 
 $api = new Binance\API("ewbwdRMN7sJw5KByJnYPOSdPXP7vCwMzBZoqOdN5BlLELnP3mQSoeQ8bninkgUx4","2tc1wt1lvKt5tPTVMPmgPca0FfTNi6vYlcDA4DgHYvgYD4ZUUygbuXPHat3pCABa");
-
-$price = $api->price("BNBBTC");
 print"<pre>";
-print_r($_ENV['apiKey']);
+print_r($_POST);
 ?>
