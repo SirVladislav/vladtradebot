1f3c4587f424ad940358a5fa4beaa15a8259aa6e
sirvladislav <sir.vladislav.programming@gmail.com>
2022-06-12 17:51:24 +0300

    2

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 require 'vendor/autoload.php';
 print_r("asdsdasdaa");
 $api = new Binance\API("ewbwdRMN7sJw5KByJnYPOSdPXP7vCwMzBZoqOdN5BlLELnP3mQSoeQ8bninkgUx4","2tc1wt1lvKt5tPTVMPmgPca0FfTNi6vYlcDA4DgHYvgYD4ZUUygbuXPHat3pCABa");
-print"<pre>";
-print_r($_POST);
+$msg = 'test';
+mail('lisi4kadrk@gmail.com', 'My Subject', $msg);
 ?>
 <script>
     console.log("yes");
